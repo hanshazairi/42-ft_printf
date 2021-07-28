27900e41b01c218b417deda26041c771348ef5c9
Hans Hazairi <hanshazairi@gmail.com>
2021-07-28 21:59:32 +0800

    chore: add p flag

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,11 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 15:08:22 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/07/28 01:59:59 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/07/28 21:55:40 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include "libft/libft.h"
@@ -18,9 +19,11 @@
 
 int	ft_printf(const char *str, ...)
 {
-	int		num;
-	char	*s;
-	va_list	ap;
+	int			num;
+	char		*s;
+	char		*tmp;
+	va_list		ap;
+	long long	llnum;
 
 	va_start(ap, str);
 	while (*str)
@@ -28,19 +31,21 @@ int	ft_printf(const char *str, ...)
 		if (*str == '%' && *str++)
 		{
 			if (*str == 'c')
-			{
-				num = va_arg(ap, int);
-				ft_putchar_fd(num, 1);
-			}
+				ft_putchar_fd(va_arg(ap, int), 1);
 			else if (*str == 's')
+				ft_putstr_fd(va_arg(ap, char *), 1);
+			else if (*str == 'p')
 			{
-				s = va_arg(ap, char *);
+				s = ft_itoa_base(va_arg(ap, long long), 16);
+				tmp = s;
+				s = ft_strjoin("0x", s);
+				free(tmp);
 				ft_putstr_fd(s, 1);
+				free(s);
 			}
 			else if (*str == 'd')
 			{
-				num = va_arg(ap, int);
-				s = ft_itoa_base(num, 10);
+				s = ft_itoa(va_arg(ap, int));
 				ft_putstr_fd(s, 1);
 				free(s);
 			}
@@ -60,9 +65,9 @@ int	main(void)
 	int		num;
 	char	*str;
 
-	num = -17;
+	num = INT_MAX;
 	str = "Hello World!";
-	printf("%c %s %d %%\n", str[0], str, num);
-	ft_printf("%c %s %d %%\n", str[0], str, num);
+	printf("%c %s %d %p %%\n", str[0], str, num, str);
+	ft_printf("%c %s %d %p %%\n", str[0], str, num, str);
 	return (0);
 }
